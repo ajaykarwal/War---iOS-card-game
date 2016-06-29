e1d4869a049a074a4575f67b333a5a7030c68a6b
Ajay Karwal <ajaykarwal@gmail.com>
2016-06-29 22:40:45 +0100

    Basic game components set up
    Running through lessons 1-12 of the playlist
    https://www.youtube.com/playlist?list=PLMRqhzcHGw1ZF7VdTt2EALt_5i6RELc0k

---
+++ War/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Ajay Karwal on 29/06/2016.
+//  Copyright © 2016 AjayKarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func playRoundTapped(sender: UIButton) {
+        self.playRoundButton.setTitle("Play Round", forState: UIControlState.Normal)
+    }
+
+}
+
